748c34f6727f470de83e6714ef153b6a3e5bbe84
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-12-07 23:50:39 +0000

    backend correctly serves homepage endpoint

--- back-end/src/App.ts
+++ back-end/src/App.ts
@@ -1,17 +1,17 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import helmet from "helmet";
-import logger from "morgan";
+// import logger from "morgan";
 const port = process.env.PORT || 3001;
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.get("/", function (req, res) {
-	res.send("Welcome to Status 418’s Rootine back end");
+	res.send("Cross-device-copy-paste default endpoint");
 });
 
 if (process.env.NODE_ENV != "test") {
