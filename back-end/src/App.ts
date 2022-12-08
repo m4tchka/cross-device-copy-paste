a77526149c52a791b4e134f6916e51b71b7d64d2
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-12-08 11:38:53 +0000

    fix process.env variables not being accessible

--- back-end/src/App.ts
+++ back-end/src/App.ts
@@ -1,23 +1,26 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import helmet from "helmet";
-// import logger from "morgan";
-const port = process.env.PORT || 3001;
+import logger from "morgan";
+import * as dotenv from "dotenv";
+dotenv.config();
+declare var process: { env: { [key: string]: string } };
+const port = process.env.PORT; /* || 3001 */
 const app = express();
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 app.use(helmet());
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.get("/", function (req, res) {
-	res.send("Cross-device-copy-paste default endpoint");
+    res.send("Cross-device-copy-paste default endpoint");
 });
 
 if (process.env.NODE_ENV != "test") {
-	app.listen(port, () => {
-		console.log(`Listening on port ${port}`);
-	});
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`);
+    });
 }
 
 export default app;
