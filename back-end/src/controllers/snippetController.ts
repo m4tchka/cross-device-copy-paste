e04ffa193236e1db08f9dec7344dfb1b0c5cf195
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-12-09 22:26:10 +0000

    add delete & patch routes

--- back-end/src/controllers/snippetController.ts
+++ back-end/src/controllers/snippetController.ts
@@ -25,7 +25,9 @@ export const getOneSnippet = async (
     }
     const specificSnippet = await Snippet.findById(req.params.id);
     if (!specificSnippet) {
-        return res.status(404).json({ error: "No snippet found ... Incorrect id" });
+        return res
+            .status(404)
+            .json({ error: "No snippet found ... Incorrect id" });
     }
     res.status(200).json(specificSnippet);
 };
@@ -44,4 +46,42 @@ export const createNewSnippet = async (
 export const deleteOneSnippet = async (
     req: express.Request,
     res: express.Response
-) => {};
+) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res
+            .status(404)
+            .json({ error: "No snippet found ... Invalid id" });
+    }
+    const deletedSnippet = await Snippet.findOneAndDelete({
+        _id: req.params.id,
+    });
+    if (!deletedSnippet) {
+        return res
+            .status(404)
+            .json({ error: "No snippet found ... Incorrect id" });
+    }
+    res.status(400).json(deletedSnippet);
+};
+
+export const updateOneSnippet = async (
+    req: express.Request,
+    res: express.Response
+) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res
+            .status(404)
+            .json({ error: "No snippet found ... Invalid id" });
+    }
+    const updatedSnippet = await Snippet.findOneAndUpdate(
+        {
+            _id: req.params.id,
+        },
+        { ...req.body }
+    );
+    if (!updatedSnippet) {
+        return res
+            .status(404)
+            .json({ error: "No snippet found ... Incorrect id" });
+    }
+    return res.status(200).json(updatedSnippet);
+};  
--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -3,18 +3,14 @@ import {
     getAllSnippets,
     createNewSnippet,
     getOneSnippet,
+    deleteOneSnippet,
+    updateOneSnippet,
 } from "../controllers/snippetController";
 const router = express.Router();
 
 router.get("/", getAllSnippets);
 router.post("/", createNewSnippet);
 router.get("/:id", getOneSnippet);
-router.delete("/:id", (req, res) => {
-    return res.json(`Delete snippet endpoint, ${req.params.id}`);
-});
-router.put("/:id", (req, res) => {
-    return res.json(
-        `Update snippet endpoint, ${req.params.id}\nNew snippet: ${req.body.snippet}`
-    );
-});
+router.delete("/:id", deleteOneSnippet);
+router.patch("/:id", updateOneSnippet);
 export default router;
